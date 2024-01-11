cb5d6d35ff67072d4c373b513555c73b99bb00f0
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-11 23:06:33 +0530

    Delete create_file_page.cpp

--- create_file_page.cpp
+++
@@ -1,9 +0,0 @@
-#include "create_file_page.h"
-#include <QLabel>
-#include <QPushButton>
-#include <QLineEdit>
-
-CreateFile::CreateFile(QMainWindow *parent) : Template(parent)
-{
-    label = new QLabel;
-}
